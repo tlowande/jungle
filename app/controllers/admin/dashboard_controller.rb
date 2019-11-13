58d67efb4872f16c3e414780ba42262c25a078df
Tamires <tamires.lowande@gmail.com>
2019-11-13 17:11:03 -0500

    adds analitics to the dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASSWORD"]
+
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
