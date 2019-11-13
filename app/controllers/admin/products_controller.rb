015e8454f4a8c8bc40c31eb1a1476f62ee86a180
Tamires <tamires.lowande@gmail.com>
2019-11-13 16:35:22 -0500

    add authentication to admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASSWORD"]
+  
+  
   def index
     @products = Product.order(id: :desc).all
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @greetings = 'hello'
     
   end
 
